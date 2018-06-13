7a5eff9d2939d4f00171bb4b1f6114a2245bff29
Chris Kelley <kelleyc718@gmail.com>
2018-06-13 16:30:55 -0500

    commit before eject

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import { compose } from "redux";
 import { Link } from "react-router-dom";
 import * as actions from "../../actions";
-
+import "../../css/login.css";
 
 class Login extends Component {
   onSubmit = formProps => {
@@ -16,39 +16,49 @@ class Login extends Component {
   render() {
     const { handleSubmit } = this.props;
     return (
-      <div className="main-content">
-        <form className="login-form" onSubmit={handleSubmit(this.onSubmit)}>
-          <label className="login-label">Email</label>
-          <Field
-            className="login-field"
-            name="email"
-            type="text"
-            component="input"
-            autoComplete="email"
-          />
+      <div className="login-container">
+          <form
+            className="login-form card"
+            onSubmit={handleSubmit(this.onSubmit)}
+          >
+            <div className="login-group">
 
-          <label className="login-label">Password</label>
-          <Field
-            className="login-field"
-            name="password"
-            type="password"
-            component="input"
-            autoComplete="none"
-          />
+                <i className="material-icons prefix">account_circle</i>
+              <label className="icon_prefix" />
+              <Field
+                className="login-field"
+                name="email"
+                type="text"
+                component="input"
+                autoComplete="email"
+              />
 
-          <div className="error">{this.props.errorMessage}</div>
 
-          <button className="login-button">Login</button>
-          <Link to="/" className="login-button">
-            Cancel
-          </Link>
-        </form>
-        <button>
-            <a href="/auth/google" className="login-button">
-                Sign In With Google
-            </a>
-        </button>
-      </div>
+              <i className="material-icons prefix">vpn_key</i>
+              <label className="icon_prefix" />
+              <Field
+                className="login-field validate"
+                name="password"
+                type="password"
+                component="input"
+                autoComplete="none"
+              />
+              <div className="error">{this.props.errorMessage}</div>
+            </div>
+
+            <div className="btn-group">
+              <button type="submit" className="login-btn button">
+                <a>Login</a>
+              </button>
+              <button className="cancel-btn button">
+                <Link to="/">Cancel</Link>
+              </button>
+                <button className="google-btn button">
+                    <a href="/auth/google">Sign In With Google</a>
+                </button>
+            </div>
+          </form>
+        </div>
     );
   }
 }
