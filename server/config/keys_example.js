e0994df4f315608d3d133729d698dfab1914b591
Chris Kelley <kelleyc718@gmail.com>
2018-09-10 22:20:13 -0500

    new keyfile

---
+++ server/config/keys_example.js
@@ -0,0 +1,20 @@
+/*
+* In order for everything to work, you will need to create a few items here.
+* Replace the items in quotes with the instructed requirements.
+* */
+
+module.exports = {
+    // Webtoken Random String
+    APP_SECRET: '#Any random string here with numbers#',
+
+    // Google Client Information
+    GOOGLE_CLIENT_ID: '#A google developer account with an endpoint will give you the ID#',
+    GOOGLE_CLIENT_SEC: '#And the security code required to use OAuth#',
+
+    // Spotify Client Information
+    SPOTIFY_CLIENT_SEC: '#A developer account with spotify will provide these items#',
+    SPOTIFY_CLIENT_ID: '#Same can be said for this id#',
+
+    // MONGODB URI
+    MONGOURI: '#MongoDB is the preferred solution for this app. But feel free use whatever SQL or NoSQL solution#'
+};
